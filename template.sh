0e8d6bb859d1f4733a0f3df261d6afe8d684fae1
erick <blackkemoji@gmail.com>
2026-06-26 15:53:44 +0300

    folder structure created

---
+++ template.sh
@@ -0,0 +1,14 @@
+#making directories
+mkdir -p src
+mkdir -p research
+#making files
+touch src/__init__.py
+touch src/helper.py
+touch src/prompt.py
+touch .env
+touch setup.py
+touch app.py
+touch research/trials.ipynb
+touch requirements.txt
+
+echo "Project structure created successfully."
